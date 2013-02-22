772f2d44f346aea5d36b358e6be2b49ee6421a3e
Joel <johnson.joel.86@gmail.com>
2013-02-22 08:41:06 -0700

    set cookie for the timezone so the server can detect the user's timezone.

---
+++ src/main/webapp/scripts/timezone.js
@@ -0,0 +1,8 @@
+Event.observe(window, 'load', function() {
+	var rightNow = new Date();
+	var jan1 = new Date(rightNow.getFullYear(), 0, 1, 0, 0, 0, 0);
+	var temp = jan1.toGMTString();
+	var jan2 = new Date(temp.substring(0, temp.lastIndexOf(" ")-1));
+	var std_time_offset = (jan1 - jan2) / (1000 * 60 * 60);
+	createCookie("timezone", std_time_offset);
+});
