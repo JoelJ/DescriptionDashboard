3f228eb34f03a7c0f354d0ec6fad81f86bd6f489
Joel <johnson.joel.86@gmail.com>
2013-04-09 10:27:25 -0600

    timezone fix

--- src/main/webapp/scripts/timezone.js
+++ src/main/webapp/scripts/timezone.js
@@ -1,8 +1,12 @@
 Event.observe(window, 'load', function() {
-	var rightNow = new Date();
-	var jan1 = new Date(rightNow.getFullYear(), 0, 1, 0, 0, 0, 0);
-	var temp = jan1.toGMTString();
-	var jan2 = new Date(temp.substring(0, temp.lastIndexOf(" ")-1));
-	var std_time_offset = (jan1 - jan2) / (1000 * 60 * 60);
-	createCookie("timezone", std_time_offset);
+	var now = new Date();
+	var nowUtcStr = now.toUTCString();
+	var semiColonIndex = nowUtcStr.indexOf(':');
+	if(semiColonIndex >= 2) {
+		var hours = nowUtcStr.substring(semiColonIndex-2, semiColonIndex);
+		var timezone = now.getHours() - parseInt(hours);
+		createCookie('timezone', timezone);
+	} else {
+		console.log("couldn't parse utc string: ", nowUtcStr);
+	}
 });
